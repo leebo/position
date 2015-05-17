31b0188b26adc40a73c476d4393512677b86c895
s8186255 <suanqi@gmail.com>
2015-05-17 14:49:00 +0800

    修订开发文档

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -1,6 +1,6 @@
 class Incident
   include Mongoid::Document
   field :customer_ids,type: Array
-  field :occurrence_time,type: DateTime
+  field :time,type: DateTime
   belongs_to :incident_type
 end
